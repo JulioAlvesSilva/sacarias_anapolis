449e7f7845e9f3988247e5a80759505129e60d4e
Julio Cesar <aps.juliocs@gmail.com>
2024-07-07 09:42:15 -0300

    07-07-2024 2

--- src/hooks/useMidiaSoci.tsx
+++ src/hooks/useMidiaSoci.tsx
@@ -1,10 +1,41 @@
+const ativarBotao = async (botao: string) => {
+    try {
+        const urlAtual = "https://server-js-phi.vercel.app/sacarias";
+        const dataHoraAtual = new Date();
+        const dataISO = dataHoraAtual.toISOString();
+        const horaISO = dataHoraAtual.toTimeString().split(' ')[0];
+
+        const response = await fetch(`${urlAtual}/salvar`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                botao: botao,
+                data: dataISO,
+                hora: horaISO
+            }),
+        });
+
+        if (response.ok) {
+
+        } else {
+            console.error('Erro ao enviar dados:', response.statusText);
+        }
+    } catch (error) {
+        console.error('Erro ao realizar requisição:', error);
+    }
+};
+
 function Instagram() {
     const insta = "https://instagram.com/sacarias_e_big_bag_anapolis?igshid=MzRlODBiNWFlZA=="
-    window.open(insta, '_blank')
+    window.open(insta, '_blank');
+    ativarBotao("Entrei no instagram");
 }
 function Facebook() {
     const face = "https://www.facebook.com/profile.php?id=100080136799599&mibextid=ZbWKwL"
-    window.open(face, '_blank')
+    window.open(face, '_blank');
+    ativarBotao("Entrei no Facebook");
 }
 function WhatsApp(mensagem: string) {
     const phoneNumber = '62992990052';
@@ -12,6 +43,7 @@ function WhatsApp(mensagem: string) {
     const baseUrl = isMobile ? `https://wa.me/${phoneNumber}?text=${encodeURIComponent(mensagem)}`
         : `https://web.whatsapp.com/send?phone=${encodeURIComponent(phoneNumber)}&text=${encodeURIComponent(mensagem)}`
     window.open(baseUrl, '_blank');
+    ativarBotao("Iniciei uma conversa pelo whats");
 }
 
 export { Facebook, Instagram, WhatsApp }
