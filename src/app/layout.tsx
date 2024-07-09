d2b38daa24b89b9092fe43f7872524f7cea97e55
Julio Cesar Alves da <aps.juliocs@gmail.com>
2024-07-09 06:31:32 -0300

    09-07-2024

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,38 @@
-  import type { Metadata } from "next";
-  import { Inter } from "next/font/google";
-  import "./globals.css";
-  import { Analytics } from '@vercel/analytics/react';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Analytics } from '@vercel/analytics/react';
 
-  const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
 
-  export const metadata: Metadata = {
-    title: "Sacarias e big bag Anápolis",
-    description: "Se você procura sacarias e Big Bags de qualidade, você esta no lugar certo! Na Sacarias e Big Bags Anápolis, oferecemos produtos de primeira linha e atendimento excepcional. Solicite seu orçamento agora e garanta as melhores sacarias e big bags do mercado.",
-    icons: "/imagens/logo.png",
-    keywords: "sacarias, Big Bags, sacarias de qualidade, Big Bags de qualidade, Sacarias e Big Bags Anápolis, sacarias Anápolis, Big Bags Anápolis, sacarias de primeira linha, sacarias para venda, sacarias e Big Bags, sacarias de alta qualidade, orçamento de sacarias, melhores sacarias do mercado"
-  };
+export const metadata: Metadata = {
+  title: "Sacarias e big bag Anápolis",
+  description: "Se você procura sacarias e Big Bags de qualidade, você esta no lugar certo! Na Sacarias e Big Bags Anápolis, oferecemos produtos de primeira linha e atendimento excepcional. Solicite seu orçamento agora e garanta as melhores sacarias e big bags do mercado.",
+  icons: "/imagens/logo.png",
+  keywords: "sacarias, Big Bags, sacarias de qualidade, Big Bags de qualidade, Sacarias e Big Bags Anápolis, sacarias Anápolis, Big Bags Anápolis, sacarias de primeira linha, sacarias para venda, sacarias e Big Bags, sacarias de alta qualidade, orçamento de sacarias, melhores sacarias do mercado"
+};
 
-  export default function RootLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    return (
-      <html lang="pt-br">
-        <body className={inter.className}>
-          {children}
-           <Analytics />
-          </body>
-      </html>
-    );
-  }
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <head>
+        <script type="application/ld+json" dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "Organization",
+            "url": "https://sacarias-anapolis.vercel.app/",
+            "logo": "/imagens/logo.png"
+          })
+        }} />
+      </head>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
+}
